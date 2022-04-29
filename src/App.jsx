b735b9a746b0d73acb124d8edf36aaec6f556738
Felipe Ramirez <felipe.ramirezrivera@gmail.com>
2022-04-29 16:56:09 -0500

    App is already working

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,9 @@ import FormOfStudents from './components/FormOfStudents.jsx'
 
 function App() {
 
-  // const [number, setNumber] = useState(0)
-
-  // const addToNumber = ()=>{
-  //   setNumber(number + 1)
-  // }
-
-  // const substractToNumber = ()=>{
-  //   setNumber(number - 1)
-  // }
-
-  
 
   return (
     <div className='container'>
-      {/* <h1>My first application</h1> */}
-      {/* <h1>{number}</h1>
-      <button onClick={addToNumber}>+</button>
-      <button onClick={substractToNumber}>-</button> */}
       <FormOfStudents />
     </div>
 
